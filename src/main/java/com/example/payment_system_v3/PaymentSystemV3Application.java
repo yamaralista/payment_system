ae4e6328944cd6cc2e4153827f4a9ed0895f78b9
Akmaral Kaiypberdieva <yamaralista1@gmail.com>
2023-10-21 18:24:51 +0600

    initial commit

---
+++ src/main/java/com/example/payment_system_v3/PaymentSystemV3Application.java
@@ -0,0 +1,13 @@
+package com.example.payment_system_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSystemV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/payment_system_v3/PaymentSystemV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payment_system_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
