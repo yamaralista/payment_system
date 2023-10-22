574d543724bda2df1e62f19db3755e2730b5cf3b
Akmaral Kaiypberdieva <yamaralista1@gmail.com>
2023-10-22 23:23:17 +0600

    change card controller

--- src/main/java/com/example/payment_system_v3/controller/CardController.java
+++ src/main/java/com/example/payment_system_v3/controller/CardController.java
@@ -8,6 +8,7 @@ import com.example.payment_system_v3.service.impl.PaymentProcessingCenterStrateg
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class CardController {
         this.paymentProcessingCenterStrategy = paymentProcessingCenterStrategy;
     }
 
+    @PostMapping("/issue")
     public ResponseEntity<String> issueCard(
             @RequestBody CardRequestDto cardRequestDto
             ) throws PaymentSystemNotFoundException,
