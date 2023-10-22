27ab2c04692630ab3525ed540e6802e51708989f
Akmaral Kaiypberdieva <yamaralista1@gmail.com>
2023-10-22 20:23:42 +0600

    adding methods to the ClientRepository

--- src/main/java/com/example/payment_system_v3/repository/ClientRepository.java
+++ src/main/java/com/example/payment_system_v3/repository/ClientRepository.java
@@ -5,6 +5,9 @@ import com.example.payment_system_v3.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> getClientById(Long id);
 }
