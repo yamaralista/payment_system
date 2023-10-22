cedebbe80c589efbef7e24849ef7f90fb6617f03
Akmaral Kaiypberdieva <yamaralista1@gmail.com>
2023-10-22 19:56:21 +0600

    adding methods to the card repository

--- src/main/java/com/example/payment_system_v3/repository/CardRepository.java
+++ src/main/java/com/example/payment_system_v3/repository/CardRepository.java
@@ -5,6 +5,10 @@ import com.example.payment_system_v3.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
+      Optional<Card> getCardByCardNumber(String cardNumber);
+      Optional<Card> getCardById(Long id);
 }
