1cb54fcd9cba3edc19e5e669a82071a678975281
Akmaral Kaiypberdieva <yamaralista1@gmail.com>
2023-10-23 15:01:34 +0600

    add documentation

--- src/main/java/com/example/payment_system_v3/sevice/ProcessingCenterStrategy.java
+++ src/main/java/com/example/payment_system_v3/sevice/ProcessingCenterStrategy.java
@@ -10,13 +10,39 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
+/**
+ * Этот интерфейс определяет контракт для стратегий обработки платежных операций, таких как выпуск карты, пополнение счета
+ * и списание средств с карты.
+ */
 public interface ProcessingCenterStrategy {
-
+    /**
+     * Метод выпуска карты на основе данных из CardRequestDto.
+     *
+     * @param cardRequestDto Данные для выпуска карты
+     * @throws InvalidCredentialsException Исключение, если данные карты недопустимы
+     * @throws ClientNotFoundException     Исключение, если клиент не найден
+     */
     void issueCard(CardRequestDto cardRequestDto) throws InvalidCredentialsException, ClientNotFoundException;
 
+    /**
+     * Метод для пополнения счета карты на указанную сумму.
+     *
+     * @param card   Карта, на которую производится пополнение
+     * @param amount Сумма пополнения
+     * @throws InvalidPinException   Исключение, если введен неверный PIN-код
+     * @throws CardNotFoundException Исключение, если карта не найдена
+     */
     @Transactional
     void topUpCard(Card card, BigDecimal amount) throws InvalidPinException, CardNotFoundException;
 
+    /**
+     * Метод для списания средств с карты на указанную сумму.
+     *
+     * @param card   Карта, с которой списываются средства
+     * @param amount Сумма списания
+     * @throws InvalidPinException   Исключение, если введен неверный PIN-код
+     * @throws CardNotFoundException Исключение, если карта не найдена
+     */
     @Transactional
     void debitCard(Card card, BigDecimal amount) throws InvalidPinException, CardNotFoundException;
 }
